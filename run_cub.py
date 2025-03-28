1f7e63415ea1d6e6a9aaef88504916bcb1cc04f9
JingHu <157672791+hujing0915@users.noreply.github.com>
2025-03-28 15:54:10 +0800

    Update run_cub.py

--- run_cub.py
+++ run_cub.py
@@ -6,7 +6,7 @@ os.system('''OMP_NUM_THREADS=64  python train_free.py --gammaD 1 --gammaG 1 \
 --gzsl --manualSeed 3483 --encoded_noise --preprocessing --cuda --image_embedding res101 --class_embedding sent \
 --nepoch 301 --ngh 4096 --ndh 4096 --lr 0.0001 --classifier_lr 0.001 --lambda1 10 --critic_iter 1 --dataroot data --dataset CUB \
  --a1 1 --a2 1 --feed_lr 0.00001 --dec_lr 0.0001 --loop 2 \
---nclass_all 200 --nclass_seen 150 --batch_size 64 --nz 312 --latent_size 312 --attSize 312 --resSize 768  \
+--nclass_all 200 --nclass_seen 150 --batch_size 64 --nz 1024 --latent_size 1024 --attSize 1024 --resSize 768  \
 --syn_num 300 --center_margin 200 --center_weight 0.5 --recons_weight 0.1 --incenter_weight 0.9  --GA_weight 1.0 \
 --SD_weight 1.0 --NI_weight 1.0 --head_number 4
 ''')
